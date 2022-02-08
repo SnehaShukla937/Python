746ea29a0daf65d97fd60a62cde16a2d5e53373f
SNEHA SHUKLA <47441375+SnehaShukla937@users.noreply.github.com>
2022-02-09 01:02:24 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,6 @@
+def main():
+    print("this is demo module")     # this will print only in this module.
+
+
+if __name__=="__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,8 @@
+from cal import *
+import demo
+
+print(add(2,3))
+print(div(4,2))
+
+print("hello",__name__)
+
